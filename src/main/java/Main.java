cb6f11a69a70fa1eb48fee3ff31a281c4c507ac1
Timo Freiberg <timo.freiberg@novatec-gmbh.de>
2017-02-24 10:54:10 +0100

    Use URL of python server for train data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,13 @@ import static spark.Spark.*;
 import com.google.gson.Gson;
 
 public class Main {
-	
-	
+
+
 	public static void main(String[] args) {
 		TicketOffice office = new TicketOffice();
-		
-		post("/hello", (req, res) -> {
+		System.out.println("Started server");
+
+		post("/reserve", (req, res) -> {
 			Gson gson = new Gson();
 			ReservationRequest request = gson.fromJson(req.body(), ReservationRequest.class);
 			Reservation reservation = office.makeReservation(request);
--- src/main/java/TicketOffice.java
+++ src/main/java/TicketOffice.java
@@ -1,15 +1,18 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class TicketOffice {
-    
+
     public Reservation makeReservation(ReservationRequest request) throws UnirestException {
 		//TODO: implement this code!
-    	Unirest.post("http://httpbin.org/post")
-    	  .queryString("name", "Mark")
-    	  .field("last", "Polo")
-    	  .asJson();
-    	System.out.println();
+	    JsonNode response =
+		    Unirest.get("http://localhost:8081/data_for_train/" + request.trainId).asJson().getBody();
+	    System.out.println(response);
+
+
+
 		return null;
     }
 
